dd5684faa49f2d864fcf6631c373a538533efae7
Dip908 <dipchondo007@gmail.com>
2026-01-09 01:01:54 +0600

    redirect the user to original url when he make request with tiny url and increase the visit count

--- index.js
+++ index.js
@@ -10,16 +10,33 @@ const app = express()
 import urlRouter from './routes/url.router.js'
 
 import { connectToDb } from './config/db.js'
+import URL from './models/Url.js'
 
 app.use(cors({
     origin: "http://localhost:3000",
     credentials: true
-})) 
+}))
 app.use(express.json())
 app.use(cookieParser())
 
 
-app.use('/api/url',urlRouter)
+app.use('/api/url', urlRouter)
+
+app.get('/:shortId', async (req, res) => {
+    const shortId = req.params.shortId
+    const entry=await URL.findOneAndUpdate({
+        shortId
+    }, {
+        $push: {
+            visitHistory: {
+                timestamp:Date.now()
+            }
+        }
+    })
+
+    res.redirect(entry.redirectURL)
+})
+
 
 app.listen(process.env.PORT, () => {
     connectToDb()
